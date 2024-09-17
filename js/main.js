377d3bea68150fb784305a34335bca52ac231d95
Jay Jay Bos <74960158+JayyBG@users.noreply.github.com>
2024-09-17 17:59:00 +0200

    Les 2

--- js/main.js
+++ js/main.js
@@ -1,25 +1,55 @@
-
+// Counter
 var count = 0;
 var counter = document.getElementById("counter");
 counter.innerText = count;
 
 var redButton = document.getElementById("button--red")
-redButton.onclick = function(){
-    if(count > -10)
-        count -= 1; 
-    else{
+redButton.onclick = function () {
+    if (count > -10)
+        count -= 1;
+    else {
         count = 0;
-}
+    }
     counter.innerText = count;
 }
 
 
 var greenButton = document.getElementById("button--green");
-greenButton.onclick = function(){
-    if(count >= 10)
-        count = 0; 
-    else{
+greenButton.onclick = function () {
+    if (count >= 10)
+        count = 0;
+    else {
         count += 1;
+        counter.innerText = count;
+    }
 }
-    counter.innerText = count;
+
+// Stopwatch
+var minutes = 0;
+var seconds = 0;
+var everySecond;
+var watch = document.getElementById("stopwatch");
+watch.innerText = minutes + ":0" + seconds;
+
+var start = document.getElementById("button--start");
+start.onclick = function () {
+    // elke seconde uitvoeren
+        everySecond = setInterval(function () {
+        seconds += 1;
+        if(seconds > 60){
+            seconds = 0
+            minutes += 1;
+        }
+        if (seconds > 9) {
+            watch.innerText = minutes + ":" + seconds;
+        } else {
+            watch.innerText = minutes + ":0" + seconds;
+        }
+    
+    }, 1000)
+}
+
+var stop = document.getElementById("button--stop");
+stop.onclick = function () {
+    clearInterval(everySecond);
 }
