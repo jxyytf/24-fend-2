89c4a10532b419b422299189cf182d240bcf58c1
jxyytf <jayybins@gmail.com>
2024-10-03 11:53:19 +0200

    les 4

--- js/main.js
+++ js/main.js
@@ -1,23 +1,15 @@
-var mobileNav = document.getElementById("mobile-nav");
-var bars = document.getElementById("button-bars")
-var body = document.getElementsByTagName("body")[0]
-var rememberY = 0;
+const addContentBtn = document.getElementById('Meer');
+const removeContentBtn = document.getElementById('Minder');
 
-mobileNav.onmousemove = function(event){
-    mobileNav.style.backgroundPosition = event.clientX/30 + "px " +event.clientY/30 + "px ";
-}
+addContentBtn.addEventListener('click', function() {
+    const newParagraph = document.createElement('p');
+    newParagraph.textContent = "Dit is meer content!";
+    document.body.appendChild(newParagraph);
+});
 
-mobileNav.onclick = function(){
-    mobileNav.style.display = "none";
-    body.style.height = "auto";
-    body.style.overflow = "scroll";
-    window.scrollTo(0,rememberY - 5);
-}
-
-bars.onclick = function(event){
-    rememberY = event.pageY;
-    mobileNav.style.display = "flex";
-    body.style.height = "100vh";
-    body.style.overflow = "hidden";
-    window.scrollTo(0,0);
-}
+removeContentBtn.addEventListener('click', function() {
+    const paragraphs = document.getElementsByTagName('p');
+    if (paragraphs.length > 0) {
+        document.body.removeChild(paragraphs[paragraphs.length - 1]);
+    }
+});
